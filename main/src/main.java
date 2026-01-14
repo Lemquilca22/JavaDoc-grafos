5d82986f26938b96bac7e45e553c1e5e35d36df1
Lemsito22 <lemquilca22@gmail.com>
2026-01-14 09:49:45 +0100

    .

---
+++ main/src/main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class main {
+    /**
+     * Funcion que comprueba si el usuario está en la base de datos
+     * @autor Lem
+     * @param user Todos los usuarios
+     * @param nom El usuario que queremos buscar.
+     * @return Devuelve la posicion del usuario, si no se encuentra devuelve -1
+     */
+    public static int usuario(String[] user, String nom){
+        Scanner sc = new Scanner(System.in);
+        nom=sc.next();
+        for (int i = 0; i < user.length; i++) {
+            if (nom.equalsIgnoreCase(user[i])){
+                    return i;
+            }
+        }
+        return -1;
+    }
+    static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+        String [] user = {"Viper_Strike","IronGhost_99","NeoHunter","Shadow_Ninja","TurboPanda","Pixel_Paladin"};
+        String [] contraseña = {"Serpiente1","Fantasma99","Caza2026","Sombra00","OsoRapido","Escudo55"};
+        int posicion = 0;
+        String nom="";
+        boolean ingresaUser=true;
+        while(ingresaUser){
+            System.out.println("Ingrese el nombre de usuario:");
+            posicion=usuario(user, nom);
+            if (posicion==-1){
+                System.out.println("No se ha encontrado ese usuario en la base de datos");
+            } else {
+                ingresaUser=false;
+            }
+        }
+        System.out.println("Usuario: "+user[posicion]);
+    }
+}
